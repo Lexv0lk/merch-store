89a7259b6807892a41d8f97ef6e35468b37c3b9c
Lexvolk <Lexvolk2004@yandex.ru>
2026-01-04 20:31:21 +0400

    feat: added postgres db via docker-compose.yml feat: added first migration to setup tables in db

---
+++ migrations/20260104154955_init_tables.sql
@@ -0,0 +1,36 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    balance INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE goods (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price INTEGER NOT NULL
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    from_user_id INTEGER REFERENCES users(id),
+    to_user_id INTEGER REFERENCES users(id),
+    amount INTEGER NOT NULL
+);
+
+CREATE TABLE purchases (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    good_id INTEGER REFERENCES goods(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS purchases;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS goods;
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
